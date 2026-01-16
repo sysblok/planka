ec5b3f6138354c927cd82f9018371bfd8ad5e999
Alex Kulikov <alexeyqu@gmail.com>
2026-01-16 02:26:21 +0000

    fix: update vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -22,8 +22,12 @@ export default defineConfig({
     },
   },
   server: {
-    port: 3000,
-    open: true,
+//    port: 3000,
+//    open: true,
+    host: true,
+    allowedHosts: [
+      'planka.sysblok.team',
+    ],
   },
   build: {
     target: browserslistToEsbuild(['>0.2%', 'not dead', 'not op_mini all']),
